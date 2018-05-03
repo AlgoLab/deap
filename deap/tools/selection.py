f44ff02fa4818ce116657357350f3e7b7651218a
Vladimir Filipovic <vladofilipovic@hotmail.com>
2018-05-03 10:27:09 +0200

    start to add fgts

--- deap/tools/selection.py
+++ deap/tools/selection.py
@@ -62,11 +62,29 @@ def selTournament(individuals, k, tournsize, fit_attr="fitness"):
     This function uses the :func:`~random.choice` function from the python base
     :mod:`random` module.
     """
+    
     chosen = []
     for i in xrange(k):
         aspirants = selRandom(individuals, tournsize)
         chosen.append(max(aspirants, key=attrgetter(fit_attr)))
     return chosen
+	
+def selTournamentFineGrained(individuals, k, tournsize, fit_attr="fitness"):
+    """Select the best individual among *tournsize* randomly chosen
+    individuals, *k* times. The list returned contains
+    references to the input *individuals*.
+    
+    :param individuals: A list of individuals to select from.
+    :param k: The number of individuals to select.
+    :param tournsize: The number of individuals participating in each tournament.
+    :param fit_attr: The attribute of individuals to use as selection criterion
+    :returns: A list of selected individuals.
+    
+    This function uses the :func:`~random.choice` function from the python base
+    :mod:`random` module.
+    .. [Luke2002fighting] Luke and Panait, 2002, Fighting bloat with 
+        nonparametric parsimony pressure   """
+
 
 def selRoulette(individuals, k, fit_attr="fitness"):
     """Select *k* individuals from the input *individuals* using *k*
